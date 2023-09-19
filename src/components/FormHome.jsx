3efaf11bc7380f7ac15de75311e6210e0aae2267
Michael Catrone <47573331+MikeCatrone@users.noreply.github.com>
2023-09-19 08:55:55 -0300

    Add files via upload

---
+++ src/components/FormHome.jsx
@@ -0,0 +1,50 @@
+
+import { useState } from "react";
+import "./FormHome.css";
+import FormUpdate from "./FormUpdate";
+
+
+
+const FormHome = () => {
+
+    // State for a given item
+    let [state, setState] = useState({
+        productName: "",
+        quantity: "",
+        key: 0,
+    })
+
+
+    // Array to hold a list of items
+    let [state2, setState2] = useState([]);
+
+
+
+    // When an li is clicked
+    const liHandler = (keyID) => {
+        
+        setState2((prev) => {
+
+            let theCopy = [...prev];
+
+            return theCopy.filter((cur) => cur.key !== keyID);
+        })
+    }
+
+
+
+    return (
+        
+        <div className="listContainer">
+            <h2>Current Shopping List</h2>
+
+            <ul>
+              {state2.length !== 0 ? state2.map((cur) => <li onClick={() => liHandler(cur.key)} key={cur.key}>{cur.productName} : {cur.quantity} </li>) : null}   
+            </ul>
+
+            <FormUpdate theState={[state, setState, state2, setState2]} />
+        </div>
+    )
+}
+
+export default FormHome;
---
+++ src/components/FormUpdate.jsx
@@ -0,0 +1,71 @@
+
+import "./FormUpdate.css";
+
+
+
+const FormUpdate = ({theState}) => {
+   
+  
+    const changeHandler = (eve) => {
+        
+        // setState() for input text values
+        theState[1]((prev) => {
+
+           prev[eve.target.name] = eve.target.value;
+           
+            return {...prev}
+        })
+    }
+
+
+    const buttonHandle = () => {
+
+        // adds a simple key value to an item
+        theState[1]((prev) => {
+
+            let copy = {...prev};
+            copy.key++;
+
+            return copy;
+        })
+        
+
+        // adds an item to an array of items
+        theState[3]((prev) => {
+
+            let copy = [...prev];
+            copy.push(theState[0]);
+
+            return copy;
+        })
+
+        
+        // resets the inputs on every button click
+        theState[1]((prev) => {
+
+            return {...prev, productName: "", quantity: ""};
+        })
+    }
+
+
+
+    return (
+
+        <div className="formUpdate">
+
+            <span className="theInputs">
+                <label htmlFor="theProduct">Product Name </label>
+                <input className="product" onChange={changeHandler} type="text" placeholder='Enter Product' id="theProduct" name="productName" value={theState[0].productName}></input>
+
+                <label className="quantity" htmlFor="theQuantity">Quantity </label>
+                <input onChange={changeHandler}  type="number" placeholder='Enter Quantity' id="theQuantity" name="quantity" value={theState[0].quantity}></input>
+            </span>
+
+            <button onClick={buttonHandle} className="addButton">Add Item</button>
+
+        </div>
+    )
+   
+}
+
+export default FormUpdate;
